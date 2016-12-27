7327e1d01095f26c54efffe354ce2ccf57f59fbf
dennisxiang <xiangdennis@gmail.com>
2016-12-26 20:53:39 -0500

    Initial Commit

---
+++ AutoDoor for iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoDoor for iOS
+//
+//  Created by Dennis Xiang on 12/26/16.
+//  Copyright © 2016 Dennis Xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
